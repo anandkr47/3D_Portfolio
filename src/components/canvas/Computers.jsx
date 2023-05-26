7f4ad19073d1b8732d818adfc05f4c33ef266951
Anand kumar <anandkumar.triden@gmail.com>
2023-05-26 23:47:29 +0530

    delted extra files

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -6,7 +6,7 @@ import CanvasLoader from "../Loader";
 
 const Computers = ({ isMobile }) => {
 //const computer = useGLTF("./desktop_pc/scene.gltf");
-const computer = useGLTF("./gaming_laptop.glb");
+const computer = useGLTF("./mac-draco.glb");
   return (
     <mesh>
       <hemisphereLight intensity={0.15} groundColor='black' />
